a63a50df5547bf694fa62e620351184c5902b29a
Lourence Linao <lourencelinao13@gmail.com>
2020-08-21 14:50:54 +0800

    homepage updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,7 +53,6 @@
             </div>
         </div>
 
-<<<<<<< HEAD
         {{-- featured projects --}}
         <div class="container text-center" style="font-size: 36px; font-weight: bold; color: #486581;">
             Featured Projects
@@ -132,9 +131,11 @@
                 {{-- icon --}}
                 <div class="col-lg-1 p-1 col-md-12 col-sm-12">
                     <div class="text-center p-3" style="background-color:#003E6B; border-radius: 10px;">
-                        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.8055 8.41415L22.8626 7.47123C22.6022 7.21081 22.1801 7.21081 21.9197 7.47123L21.4485 7.94248L20.2443 6.73831C20.4789 5.8504 20.2593 4.86748 19.563 4.17123L17.6776 2.28581C15.0743 -0.31752 10.853 -0.31752 8.24929 2.28581L12.0205 4.17123V4.95248C12.0205 5.65956 12.3014 6.3379 12.8018 6.8379L14.8493 8.8854C15.5455 9.58165 16.5285 9.80123 17.4164 9.56665L18.6205 10.7708L18.1493 11.2421C17.8889 11.5025 17.8889 11.9246 18.1493 12.185L19.0922 13.1279C19.3526 13.3883 19.7747 13.3883 20.0351 13.1279L23.8064 9.35665C24.066 9.09665 24.066 8.67456 23.8055 8.41415ZM11.8589 7.78081C11.7047 7.62665 11.5739 7.45623 11.4485 7.2829L0.819294 17.2066C-0.246123 18.2016 -0.274873 19.8812 0.755544 20.9121C1.78596 21.9429 3.46596 21.9141 4.46096 20.8483L14.383 10.2204C14.218 10.0991 14.0539 9.97581 13.9064 9.82831L11.8589 7.78081Z" fill="white"/>
+                        <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.4 8.79966H18C17.5455 8.79966 17.1296 9.05653 16.9264 9.46303L15.9311 11.4539L13.158 1.28391C13.0121 0.750282 12.5183 0.380532 11.9674 0.400407C11.4135 0.415782 10.9425 0.806907 10.8263 1.34841L8.2335 13.4489L7.15388 9.66966C7.0065 9.15478 6.53588 8.79966 6 8.79966H0.6C0.2685 8.79966 0 9.06816 0 9.39966V10.5997C0 10.9312 0.2685 11.1997 0.6 11.1997H5.09475L7.2465 18.7293C7.39425 19.2457 7.866 19.5993 8.39963 19.5993C8.41313 19.5993 8.427 19.5993 8.4405 19.5985C8.99063 19.5798 9.45825 19.189 9.57375 18.6505L12.1403 6.67303L14.4424 15.115C14.5064 15.3494 14.6401 15.5589 14.8257 15.7157C15.0113 15.8725 15.2401 15.9693 15.4819 15.9933C15.9754 16.0383 16.4509 15.7799 16.6736 15.3359L18.7418 11.1997H23.4C23.7315 11.1997 24 10.9312 24 10.5997V9.39966C24 9.06816 23.7315 8.79966 23.4 8.79966Z" fill="white"/>
                         </svg>
+                            
+                            
                     </div>
                 </div>
 
@@ -199,8 +200,9 @@
                 <div class="col-lg-1 p-1 col-md-12 col-sm-12">
                     <div class="text-center p-3" style="background-color:#003E6B; border-radius: 10px;">
                         <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.8055 8.41415L22.8626 7.47123C22.6022 7.21081 22.1801 7.21081 21.9197 7.47123L21.4485 7.94248L20.2443 6.73831C20.4789 5.8504 20.2593 4.86748 19.563 4.17123L17.6776 2.28581C15.0743 -0.31752 10.853 -0.31752 8.24929 2.28581L12.0205 4.17123V4.95248C12.0205 5.65956 12.3014 6.3379 12.8018 6.8379L14.8493 8.8854C15.5455 9.58165 16.5285 9.80123 17.4164 9.56665L18.6205 10.7708L18.1493 11.2421C17.8889 11.5025 17.8889 11.9246 18.1493 12.185L19.0922 13.1279C19.3526 13.3883 19.7747 13.3883 20.0351 13.1279L23.8064 9.35665C24.066 9.09665 24.066 8.67456 23.8055 8.41415ZM11.8589 7.78081C11.7047 7.62665 11.5739 7.45623 11.4485 7.2829L0.819294 17.2066C-0.246123 18.2016 -0.274873 19.8812 0.755544 20.9121C1.78596 21.9429 3.46596 21.9141 4.46096 20.8483L14.383 10.2204C14.218 10.0991 14.0539 9.97581 13.9064 9.82831L11.8589 7.78081Z" fill="white"/>
+                            <path d="M23.9629 18.4688L19.2296 0.794167C19.0917 0.279167 18.5671 -0.1325 17.9229 0.04L17.2792 0.2125C17.0217 0.281667 16.7988 0.479167 16.61 0.805833C16.0254 1.095 14.4308 1.52292 13.7767 1.565C13.45 1.37625 13.1579 1.31667 12.9004 1.38583L12.2563 1.55833C12.1554 1.58542 12.0813 1.63958 12 1.68667V1.33333C12 0.597083 11.4029 0 10.6667 0H8C7.26375 0 6.66667 0.597083 6.66667 1.33333V4H12L16.2358 20.5392C16.3738 21.0542 16.8983 21.4658 17.5425 21.2933L18.1863 21.1208C18.4438 21.0517 18.6667 20.8542 18.8554 20.5275C19.435 20.2408 21.0296 19.8108 21.6888 19.7683C22.0154 19.9571 22.3075 20.0167 22.565 19.9475L23.2092 19.775C23.8529 19.6025 24.1012 18.9838 23.9629 18.4688ZM18.3629 18.1746L14.5267 3.84875L17.1025 3.15875L20.9388 17.4846L18.3629 18.1746ZM0 16H5.33333V5.33333H0V16ZM4 0H1.33333C0.597083 0 0 0.597083 0 1.33333V4H5.33333V1.33333C5.33333 0.597083 4.73625 0 4 0ZM0 20C0 20.7362 0.597083 21.3333 1.33333 21.3333H4C4.73625 21.3333 5.33333 20.7362 5.33333 20V17.3333H0V20ZM6.66667 16H12V5.33333H6.66667V16ZM6.66667 20C6.66667 20.7362 7.26375 21.3333 8 21.3333H10.6667C11.4029 21.3333 12 20.7362 12 20V17.3333H6.66667V20Z" fill="white"/>
                         </svg>
+                            
                     </div>
                 </div>
 
@@ -257,118 +259,20 @@
                         </div>
                     </div>
                 </div>
-=======
-        <div class="featured-projects-container container">
-            <h1 class="text-center">Featured Projects</h1>
-
-            {{-- card group for featured projects --}}
-            <div class="d-flex flex-column card-group featured-group bg-white p-2">
-
-                {{-- added two example cards while logic for looping thru data is not yet implemented --}}
-                
-                {{-- card start --}}
-                <div class="card mb-3" >
-                    <a href="" class="homecard-links">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                {{-- <img src="..." class="card-img" alt="..."> --}}
-                                <div class="type-icon-container bg-dark"></div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <div>
-                                        <h5 class="card-title w-50 d-inline">Project Title <span class="badge badge-warning">Ongoing</span></h5>
-                                            <span class="float-right d-inline">
-                                                <i class="far fa-smile"></i>
-                                                <span class="badge badge-light">50+</span>
-
-                                                <i class="far fa-frown"></i>
-                                                <span class="badge badge-light">20+</span>
-                                            </span>
-                                    </div>
-
-                                    <div>
-                                        <p class="card-text d-inline" style="margin-right: 2em;"><small class="text-muted">P 1.5 Million</small></p>
-                                        <p class="card-text d-inline">
-                                            <i class="fas fa-map-marker-alt"></i>
-                                            <small class="text-muted">location</small>
-                                        </p>
-                                        <p class="float-right d-inline">
-                                            <i class="far fa-calendar"></i>
-                                            <small class="text-muted">August 20, 2020</small>
-                                        </p>
-                                    </div>
-                                
-                                </div>
-                            </div>
-                        </div>
-                        </a>
-                </div>
-                {{-- card end --}}
-                
-                
-
-                {{-- card start --}}
-                 <div class="card mb-3" >
-                    <a href="" class="homecard-links">
-                        <div class="row no-gutters">
-                            <div class="col-md-4">
-                                {{-- <img src="..." class="card-img" alt="..."> --}}
-                                <div class="type-icon-container bg-dark"></div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <div>
-                                        <h5 class="card-title w-50 d-inline">Project Title <span class="badge badge-success">Finished</span></h5>
-                                            <span class="float-right d-inline">
-                                                <i class="far fa-smile"></i>
-                                                <span class="badge badge-light">50+</span>
-
-                                                <i class="far fa-frown"></i>
-                                                <span class="badge badge-light">20+</span>
-                                            </span>
-                                    </div>
-
-                                    <div>
-                                        <p class="card-text d-inline" style="margin-right: 2em;"><small class="text-muted">P 1.5 Million</small></p>
-                                        <p class="card-text d-inline">
-                                            <i class="fas fa-map-marker-alt"></i>
-                                            <small class="text-muted">location</small>
-                                        </p>
-                                        <p class="float-right d-inline">
-                                            <i class="far fa-calendar"></i>
-                                            <small class="text-muted">August 20, 2020</small>
-                                        </p>
-                                    </div>
-                                
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                {{-- card end --}}
-                <h2 class="text-center">
-                    <a class="btn btn-light" href="#" role="button">View All Projects</a>
-                </h2>
->>>>>>> origin
             </div>
 
-<<<<<<< HEAD
             {{-- new row --}}
             <div class="row d-flex align-items-start pb-5 mb-5" style="border-bottom: 2px solid #D9E2EC;">
 
                 {{-- icon --}}
                 <div class="col-lg-1 p-1 col-md-12 col-sm-12">
                     <div class="text-center p-3" style="background-color:#003E6B; border-radius: 10px;">
-                        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.8055 8.41415L22.8626 7.47123C22.6022 7.21081 22.1801 7.21081 21.9197 7.47123L21.4485 7.94248L20.2443 6.73831C20.4789 5.8504 20.2593 4.86748 19.563 4.17123L17.6776 2.28581C15.0743 -0.31752 10.853 -0.31752 8.24929 2.28581L12.0205 4.17123V4.95248C12.0205 5.65956 12.3014 6.3379 12.8018 6.8379L14.8493 8.8854C15.5455 9.58165 16.5285 9.80123 17.4164 9.56665L18.6205 10.7708L18.1493 11.2421C17.8889 11.5025 17.8889 11.9246 18.1493 12.185L19.0922 13.1279C19.3526 13.3883 19.7747 13.3883 20.0351 13.1279L23.8064 9.35665C24.066 9.09665 24.066 8.67456 23.8055 8.41415ZM11.8589 7.78081C11.7047 7.62665 11.5739 7.45623 11.4485 7.2829L0.819294 17.2066C-0.246123 18.2016 -0.274873 19.8812 0.755544 20.9121C1.78596 21.9429 3.46596 21.9141 4.46096 20.8483L14.383 10.2204C14.218 10.0991 14.0539 9.97581 13.9064 9.82831L11.8589 7.78081Z" fill="white"/>
+                        <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.4 8.79966H18C17.5455 8.79966 17.1296 9.05653 16.9264 9.46303L15.9311 11.4539L13.158 1.28391C13.0121 0.750282 12.5183 0.380532 11.9674 0.400407C11.4135 0.415782 10.9425 0.806907 10.8263 1.34841L8.2335 13.4489L7.15388 9.66966C7.0065 9.15478 6.53588 8.79966 6 8.79966H0.6C0.2685 8.79966 0 9.06816 0 9.39966V10.5997C0 10.9312 0.2685 11.1997 0.6 11.1997H5.09475L7.2465 18.7293C7.39425 19.2457 7.866 19.5993 8.39963 19.5993C8.41313 19.5993 8.427 19.5993 8.4405 19.5985C8.99063 19.5798 9.45825 19.189 9.57375 18.6505L12.1403 6.67303L14.4424 15.115C14.5064 15.3494 14.6401 15.5589 14.8257 15.7157C15.0113 15.8725 15.2401 15.9693 15.4819 15.9933C15.9754 16.0383 16.4509 15.7799 16.6736 15.3359L18.7418 11.1997H23.4C23.7315 11.1997 24 10.9312 24 10.5997V9.39966C24 9.06816 23.7315 8.79966 23.4 8.79966Z" fill="white"/>
                         </svg>
+                            
                     </div>
                 </div>
-=======
-        </div>
-        {{-- featured-projects end --}}
->>>>>>> origin
 
                 <div class="col-lg-4 col-md-12 col-sm-12 col-12">
                     <div class="" style="font-size: 24px; color: #486581;">
@@ -405,7 +309,6 @@
                     </div>
                 </div>
 
-<<<<<<< HEAD
                 <div class="col-lg col-md-4 col-sm-6 col-6">
                     <div class="row pt-3">
                         <div class="ml-auto d-flex align-items-center">
@@ -422,26 +325,20 @@
                             </svg>
                             <div class="ml-1" style="font-size: 16px; color: #9FB3C8;">May 16, 20XX</div>
                         </div>
-=======
-                <div class="card bg-dark text-white">
-                    {{-- <img src="..." class="card-img" alt="..."> --}}
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">org image</h5>
->>>>>>> origin
                     </div>
                 </div>
             </div>
 
-<<<<<<< HEAD
             {{-- new row --}}
             <div class="row d-flex align-items-start pb-5 mb-5" style="border-bottom: 2px solid #D9E2EC;">
 
                 {{-- icon --}}
                 <div class="col-lg-1 p-1 col-md-12 col-sm-12">
                     <div class="text-center p-3" style="background-color:#003E6B; border-radius: 10px;">
-                        <svg width="24" height="22" viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M23.8055 8.41415L22.8626 7.47123C22.6022 7.21081 22.1801 7.21081 21.9197 7.47123L21.4485 7.94248L20.2443 6.73831C20.4789 5.8504 20.2593 4.86748 19.563 4.17123L17.6776 2.28581C15.0743 -0.31752 10.853 -0.31752 8.24929 2.28581L12.0205 4.17123V4.95248C12.0205 5.65956 12.3014 6.3379 12.8018 6.8379L14.8493 8.8854C15.5455 9.58165 16.5285 9.80123 17.4164 9.56665L18.6205 10.7708L18.1493 11.2421C17.8889 11.5025 17.8889 11.9246 18.1493 12.185L19.0922 13.1279C19.3526 13.3883 19.7747 13.3883 20.0351 13.1279L23.8064 9.35665C24.066 9.09665 24.066 8.67456 23.8055 8.41415ZM11.8589 7.78081C11.7047 7.62665 11.5739 7.45623 11.4485 7.2829L0.819294 17.2066C-0.246123 18.2016 -0.274873 19.8812 0.755544 20.9121C1.78596 21.9429 3.46596 21.9141 4.46096 20.8483L14.383 10.2204C14.218 10.0991 14.0539 9.97581 13.9064 9.82831L11.8589 7.78081Z" fill="white"/>
+                        <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.4 8.79966H18C17.5455 8.79966 17.1296 9.05653 16.9264 9.46303L15.9311 11.4539L13.158 1.28391C13.0121 0.750282 12.5183 0.380532 11.9674 0.400407C11.4135 0.415782 10.9425 0.806907 10.8263 1.34841L8.2335 13.4489L7.15388 9.66966C7.0065 9.15478 6.53588 8.79966 6 8.79966H0.6C0.2685 8.79966 0 9.06816 0 9.39966V10.5997C0 10.9312 0.2685 11.1997 0.6 11.1997H5.09475L7.2465 18.7293C7.39425 19.2457 7.866 19.5993 8.39963 19.5993C8.41313 19.5993 8.427 19.5993 8.4405 19.5985C8.99063 19.5798 9.45825 19.189 9.57375 18.6505L12.1403 6.67303L14.4424 15.115C14.5064 15.3494 14.6401 15.5589 14.8257 15.7157C15.0113 15.8725 15.2401 15.9693 15.4819 15.9933C15.9754 16.0383 16.4509 15.7799 16.6736 15.3359L18.7418 11.1997H23.4C23.7315 11.1997 24 10.9312 24 10.5997V9.39966C24 9.06816 23.7315 8.79966 23.4 8.79966Z" fill="white"/>
                         </svg>
+                            
                     </div>
                 </div>
 
@@ -506,26 +403,6 @@
                         <button class="btn" style="color:#486581; font-size: 16px; font-weight: bold;">
                             View All Projects...
                         </button>
-=======
-                <div class="card bg-dark text-white">
-                    {{-- <img src="..." class="card-img" alt="..."> --}}
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">org image</h5>
-                    </div>
-                </div>
-
-                <div class="card bg-dark text-white">
-                    {{-- <img src="..." class="card-img" alt="..."> --}}
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">org image</h5>
-                    </div>
-                </div>
-
-                <div class="card bg-dark text-white">
-                    {{-- <img src="..." class="card-img" alt="..."> --}}
-                    <div class="card-img-overlay">
-                      <h5 class="card-title">org image</h5>
->>>>>>> origin
                     </div>
                 </div>
             </div>
