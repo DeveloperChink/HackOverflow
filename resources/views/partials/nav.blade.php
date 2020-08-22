b2f2d281180319f476191e005560dcdec5edf7c7
Lourence Linao <lourencelinao13@gmail.com>
2020-08-22 20:45:04 +0800

    logo added

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">  
         {{-- to change app name, change it inside .env --}}
-    <a class="navbar-brand" href="{{url('/')}}">{{config('app.name')}}</a>
+    <a class="navbar-brand" href="{{url('/')}}">
+        <img src="{{asset('uploads/adarna.png')}}" width="30" height="30" class="d-inline-block align-top" alt="">
+        <a class="navbar-brand" href="{{url('/')}}">{{config('app.name')}}</a>
+    </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
